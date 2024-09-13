67e32401117e3d8c01c8f589fc1aaafde80cffc0
anirudhkala110 <anirudhkala110@gmail.com>
2024-09-14 03:59:43 +0530

    Updated New Data

--- src/Utils/Footer2.jsx
+++ src/Utils/Footer2.jsx
@@ -2,7 +2,39 @@ import React from 'react'
 
 const Footer2 = () => {
     return (
-        <div>Footer2</div>
+        <div className='container'>
+            <div className='row col-12'>
+                <div className='col-4 px-2'>
+                    <p className='text-uppercase text-white fw-semibold mb-2'>Our Work</p>
+                    <div>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Creative Portfolio</a></p>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Case Study</a></p>
+                    </div>
+                </div>
+                <div className='col-4 px-2'>
+                    <p className='text-uppercase text-white fw-semibold mb-2'>Media</p>
+                    <div>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Blogs</a></p>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>News</a></p>
+                    </div>
+                </div>
+                <div className='col-4 px-2'>
+                    <p className='text-uppercase text-white mb-2 fw-semibold'>Located At</p>
+                    <div>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Navi Mumbai</a></p>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Mumbai</a></p>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Gurugram</a></p>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Bengaluru</a></p>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>Dubai</a></p>
+                        <p className='text-white-50' style={{ marginBottom: '10px' }}><a href='#' className='text-decoration-none'>United Kingdom</a></p>
+                    </div>
+                </div>
+                <div className='col-12 text-start'>
+                    <p className='text-uppercase text-white mb-2 fw-semibold'><a href='#' className='text-decoration-none'>Job Openings</a></p>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
