491d828f8edc44a65d42e48ab452c54821fea6ef
anirudhkala110 <anirudhkala110@gmail.com>
2024-09-15 04:36:07 +0530

    updated our journey

--- src/Components/BenchMark.jsx
+++ src/Components/BenchMark.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const BenchMark = () => {
     return (
         <div className='bg-dark'>
-            <div className='px-2 align-items-center text-white py-3 '>
+            <div className='container px-2 align-items-center text-white py-3 '>
                 <div className='row py-2'>
-                    <div className='col-sm-12 col-md-6'>
+                    <div className='col-sm-12 col-md-8'>
                         <p className='fs-3' style={{ fontWeight: '700' }}>Benchmark your company’s digital maturity with a quick 10 minutes assessment</p>
                     </div>
-                    <div className='col-sm-12 col-md-6'>
+                    <div className='col-sm-12 col-md-4'>
                         <div className='form-group d-flex align-items-center justify-between py-3'>
                             <button className='btn bg-white py-3 fs-5 mx-2 subscribeButton text-dark text-uppercase'>TRY OUR Assessment <i className='bi bi-arrow-right'></i></button>
                         </div>
